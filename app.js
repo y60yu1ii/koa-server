68ffd48c8feb5f2b1fed06642fa6141e43c7acb7
yaoyu <yaoyu@fishare.de>
2019-10-16 15:54:20 +0800

    jshint8 & add chatbot reply

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/*jshint esversion: 6 */
+/*jshint esversion: 8 */
 
 const Koa = require('koa');
 const Router = require('koa-router');
@@ -12,6 +12,18 @@ const channelToken = require('./token.js').channelToken;
 const replyurl = require('./token.js').utl;
 
 const port = 80;
+
+router.post('/chatbot', async function (ctx) {
+    var msg = JSON.parse(ctx.request.body);
+    console.log(msg);
+    var replyToken = msg.events[0].replyToken;
+    var userMessage = msg.events[0].message.text;
+
+    if (typeof replyToken === 'undefined') {
+        return;
+    }
+
+});
 
 router.post('/open', async function (ctx) {
     console.log('open/ POST data = ' + JSON.stringify(ctx.request.body));
@@ -30,6 +42,5 @@ app.use(router.allowedMethods());
 
 server.listen(port, () => {
     console.log('listening on *:' + port);
-    console.log('channel token is ' + channelToken);
 });
 
